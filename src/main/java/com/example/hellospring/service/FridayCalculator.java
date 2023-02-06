70b3815b3ca71bcabd573350d043ae29b29ad689
jarltuxen <jarl.tuxen@gmail.com>
2023-02-06 13:46:04 +0100

    Ændret erdetfredag

--- src/main/java/com/example/hellospring/service/FridayCalculator.java
+++ src/main/java/com/example/hellospring/service/FridayCalculator.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 
 public class FridayCalculator {
 
+    //FridayCalculator branch
     public String erdetfredag(){
         LocalDate today = LocalDate.now();
         int q = today.getDayOfMonth(); //day of the month (1-31)
